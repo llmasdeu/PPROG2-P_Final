89e5354b1c9ff9c882678427257d4348fa1d39b7
Lluís Masdeu <lluismasdeu@MacBook-Pro-de-Lluis.local>
2017-04-17 12:10:07 +0200

    Habilitada la conexión con el repositorio en Android Studio.

--- PPROG2-P_Final/app/src/main/java/com/example/lluismasdeu/pprog2_p_final/MainActivity.java
+++ PPROG2-P_Final/app/src/main/java/com/example/lluismasdeu/pprog2_p_final/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.lluismasdeu.pprog2_p_final;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Localizamos los componentes en el Layout.
+        username = (EditText) findViewById(R.id.username_editText);
+        password = (EditText) findViewById(R.id.password_editText);
+    }
+
+    public void onLoginButtonClick(View view) {
+        // TODO
+    }
+
+    public void onRegisterButtonClick(View view) {
+        // TODO
     }
 }
