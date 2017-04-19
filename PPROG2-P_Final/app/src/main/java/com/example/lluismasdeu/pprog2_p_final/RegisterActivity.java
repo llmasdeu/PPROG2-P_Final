08ccbf0c7c6c541e54ee43f712ec38f70a029d98
Lluís Masdeu <lluismasdeu@MacBook-Pro-de-Lluis.local>
2017-04-20 01:18:59 +0200

    Modificados los Layout de la actividad principal y la actividad de registro de usuario. Localizados los componentes en la actividad de registro.

--- PPROG2-P_Final/app/src/main/java/com/example/lluismasdeu/pprog2_p_final/RegisterActivity.java
+++ PPROG2-P_Final/app/src/main/java/com/example/lluismasdeu/pprog2_p_final/RegisterActivity.java
@@ -1,5 +1,7 @@
 package com.example.lluismasdeu.pprog2_p_final;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,21 +10,37 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 
+import java.io.InputStream;
+
 public class RegisterActivity extends AppCompatActivity {
     private EditText name;
     private EditText surname;
-    private ImageView image;
-    private EditText mail;
+    private ImageView profileImage;
+    private EditText email;
     private EditText password;
-    private EditText repeatPassword;
+    private EditText confirmPassword;
     private RadioButton male;
     private RadioButton female;
-    private CheckBox termins;
+    private CheckBox terms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // Localizamos los componentes en el Layout.
+        name = (EditText) findViewById(R.id.name_editText);
+        surname = (EditText) findViewById(R.id.surname_editText);
+        profileImage = (ImageView) findViewById(R.id.profile_imageView);
+        email = (EditText) findViewById(R.id.email_editText);
+        password = (EditText) findViewById(R.id.passwordRegister_editText);
+        confirmPassword = (EditText) findViewById(R.id.confirmPasswordRegister_editText);
+        male = (RadioButton) findViewById(R.id.male_radioButton);
+        female = (RadioButton) findViewById(R.id.female_radioButton);
+        terms = (CheckBox) findViewById(R.id.terms_checkBox);
+
+        // Definimos la imagen de perfil predefinida.
+        setDefaultProfilePhoto();
     }
 
     public void onTakePictureButtonClick(View view) {
@@ -31,5 +49,15 @@ public class RegisterActivity extends AppCompatActivity {
 
     public void orRegisterUserButtonClick(View view) {
         // TODO
+    }
+
+    /**
+     * Método encargada de cargar la imagen de perfil por defecto.
+     */
+    private void setDefaultProfilePhoto() {
+        InputStream iStream = getResources().openRawResource(R.raw.basic_profile_photo);
+        Bitmap defaultImage = BitmapFactory.decodeStream(iStream);
+
+        profileImage.setImageBitmap(defaultImage);
     }
 }
