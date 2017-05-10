046eed45de3c20c9b6b12fbcc7fdb890ed501fd7
EloyLopez <towel.winger@gmail.com>
2017-05-10 12:52:46 +0200

    Tabs layout
    - inicio implementacion tabs layout

--- PPROG2-P_Final/app/src/main/java/com/example/lluismasdeu/pprog2_p_final/activities/ResultsActivity.java
+++ PPROG2-P_Final/app/src/main/java/com/example/lluismasdeu/pprog2_p_final/activities/ResultsActivity.java
@@ -1,6 +1,8 @@
 package com.example.lluismasdeu.pprog2_p_final.activities;
 
 import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,6 +12,9 @@ import android.view.View;
 import android.widget.Spinner;
 
 import com.example.lluismasdeu.pprog2_p_final.R;
+import com.example.lluismasdeu.pprog2_p_final.adapters.TabsAdapter;
+
+import java.util.ArrayList;
 
 public class ResultsActivity extends AppCompatActivity {
 
@@ -22,6 +27,8 @@ public class ResultsActivity extends AppCompatActivity {
         //Recuperamos valor del spinner action bar
         filter=(Spinner) findViewById(R.id.menuSort);
 
+        createTabs();
+
     }
     // Intenet para ProfileActivity
     public void onClickProfile(View view){
@@ -32,6 +39,18 @@ public class ResultsActivity extends AppCompatActivity {
     public void onClickFavorite(View view){
         Intent intent=new Intent(this,FavoriteActivity.class);
         startActivity(intent);
+    }
+    private void createTabs() {
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        ArrayList<TabsAdapter.TabEntry> entries = new ArrayList<>();
+       //entries.add(new TabsAdapter.TabEntry(new ListViewFragment(), "All"));
+        // entries.add(new TabsAdapter.TabEntry(new ListViewFragment(), "Only open"));
+
+        TabsAdapter adapter = new TabsAdapter(getSupportFragmentManager(), entries);
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
 //TODO opcion para filtro de resultados
