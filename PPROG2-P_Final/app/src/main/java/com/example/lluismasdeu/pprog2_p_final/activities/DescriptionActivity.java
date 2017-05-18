054f666aa67a4d3f7d2502cdf996e56df0279aa1
EloyLopez <towel.winger@gmail.com>
2017-05-18 11:23:44 +0200

    Spinner results
    - Spinner se llena(no terminado)

--- PPROG2-P_Final/app/src/main/java/com/example/lluismasdeu/pprog2_p_final/activities/DescriptionActivity.java
+++ PPROG2-P_Final/app/src/main/java/com/example/lluismasdeu/pprog2_p_final/activities/DescriptionActivity.java
@@ -1,21 +1,75 @@
 package com.example.lluismasdeu.pprog2_p_final.activities;
 
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-
+import android.widget.TextView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.lluismasdeu.pprog2_p_final.R;
+import org.json.JSONArray;
+import org.json.JSONException;
+import java.util.Objects;
 
 public class DescriptionActivity extends AppCompatActivity {
-
+    JsonArrayRequest jsArrayRequest;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description);
+        textView=(TextView) findViewById(R.id.text_view);
         getSupportActionBar().setTitle("");
+        final String nombre = getIntent().getStringExtra("name");
+        String url ="http://testapi-pprog2.azurewebsites.net/api/locations.php?method=getLocations";
+        RequestQueue queue = Volley.newRequestQueue(this);
+        jsArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        try {
+                            JSONArray search = response;
+                            for (int i = 0; i < search.length(); i++) {
+                                if(Objects.equals(nombre, search.getJSONObject(i).getString("name")))
+                                {
+                                   //textView.setText(search.getJSONObject(i).getString("type"));
+
+                                }
+
+                            }
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                       }
+
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                }
+        );
+
+        queue.add(jsArrayRequest);
+
     }
     public boolean onCreateOptionsMenu(Menu menu) {
         //Mostramos actionBar
--- PPROG2-P_Final/app/src/main/java/com/example/lluismasdeu/pprog2_p_final/activities/ResultsActivity.java
+++ PPROG2-P_Final/app/src/main/java/com/example/lluismasdeu/pprog2_p_final/activities/ResultsActivity.java
@@ -1,26 +1,35 @@
 package com.example.lluismasdeu.pprog2_p_final.activities;
 
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.lluismasdeu.pprog2_p_final.R;
 import com.example.lluismasdeu.pprog2_p_final.adapters.TabAdapter;
 import com.example.lluismasdeu.pprog2_p_final.fragments.ListViewFragment;
-
+import org.json.JSONArray;
+import org.json.JSONException;
 import java.util.ArrayList;
 
 public class ResultsActivity extends AppCompatActivity {
 
     Spinner filter;
-    private String searchParameter="Barcelona";
-
+    private String searchParameter="Lleida";
+    ArrayList<String> types=new ArrayList<String>();
+    JsonArrayRequest jsArrayRequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +38,47 @@ public class ResultsActivity extends AppCompatActivity {
         //Recuperamos valor del spinner action bar
         filter=(Spinner) findViewById(R.id.menuSort);
 
+        //creamos tabs
         createTabs();
 
+        //llenamos spinner
+        types.add("Filter");
+        String url ="http://testapi-pprog2.azurewebsites.net/api/locations.php?method=getLocations&s=";
+        RequestQueue queue = Volley.newRequestQueue(this);
+        jsArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                url+searchParameter,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        try {
+                            JSONArray search = response;
+                            for (int i = 0; i < search.length(); i++) {
+                                if(!types.contains(search.getJSONObject(i).getString("type")))
+                                {
+
+                                    types.add(search.getJSONObject(i).getString("type"));
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                }
+        );
+
+        queue.add(jsArrayRequest);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,types);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        filter.setAdapter(adapter);
 
     }
     // Intenet para ProfileActivity
